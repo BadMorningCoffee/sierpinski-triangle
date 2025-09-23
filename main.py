4957c6e921af207ead646f1b9eeec8bb086c3383
BadMorningCoffee <logan.magill.18@gmail.com>
2025-09-22 23:24:21 -0400

    Updated main to include a better template for starting work using plotly

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import plotly.express as px
-fig = px.scatter(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])
+import pandas as pd
+
+# Sample data
+data = {'x_values': [1, 2, 3, 4, 5],
+        'y_values': [2, 4, 1, 5, 3],
+        'category': ['A', 'B', 'A', 'C', 'B']}
+df = pd.DataFrame(data)
+
+# Create a scatter plot
+fig = px.scatter(df, x="x_values", y="y_values", color="category",
+                 title="Interactive Scatter Plot with Plotly Express")
 fig.show()
